e050924687cfa361bdd30513d05e36d1767619ea
Matthew Knowles <mattknowles314@gmail.com>
2023-06-25 14:29:29 +0100

    Reads all the IPD data from /Data

---
+++ R/handle_IPD.R
@@ -0,0 +1,8 @@
+system("python3 src/IPD_handler.py")
+
+for (i in list.files("Data/IPD/", pattern = ".csv")) {
+  assign(
+    stringr::str_remove(i, ".csv"),
+    read.csv(paste0("Data/IPD/",i))
+  )
+}
