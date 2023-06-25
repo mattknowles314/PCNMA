bc784d3ae8d0a55e8c292a1a1819d8bd7e6b2594
Matthew Knowles <mattknowles314@gmail.com>
2023-06-25 15:42:24 +0100

    Update IPD handling

--- R/handle_IPD.R
+++ R/handle_IPD.R
@@ -3,6 +3,7 @@ system("python3 src/IPD_handler.py")
 for (i in list.files("Data/IPD/", pattern = ".csv")) {
   assign(
     stringr::str_remove(i, ".csv"),
-    read.csv(paste0("Data/IPD/",i))
-  )
+    read.csv(paste0("Data/IPD/",i)) %>% 
+      mutate(status = ifelse(censored == TRUE, 0, 1))
+  ) 
 }
